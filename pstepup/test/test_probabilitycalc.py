118fad6067e3e78d6211c5961b2abbeba33a2c21
Stu Baker <stubakers@gmail.com>
2019-05-31 18:14:50 -0600

    adding tests, updating readme

---
+++ pstepup/test/test_probabilitycalc.py
@@ -0,0 +1,44 @@
+
+from pstepup import probabilitycalc, appdata
+
+
+def test_expected_value():
+    rates_and_pulls = { 1 : 1, 0.03 : 100 }
+    assert probabilitycalc.expected_value(rates_and_pulls) == 4
+    
+    actual = probabilitycalc.expected_value({ 0.25 : 1, 0.01 : 9, 0.02 : 4, 0.015 : 1, 0.05 : 10 })
+    assert round(actual, 3) == 0.935
+
+
+def test_prob_for_exactly_x():
+    assert probabilitycalc.prob_for_exactly_x(0.03, 1) == { 0 : 0.97, 1 : 0.03 }
+    assert probabilitycalc.prob_for_exactly_x(1.0, 1) == { 0 : 0, 1 : 1 }
+    assert probabilitycalc.prob_for_exactly_x(1.0, 4) == { 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 1 }
+
+    actual = { k:round(v,7) for k,v in probabilitycalc.prob_for_exactly_x(0.02, 4).iteritems() }
+    assert actual == { 0 : 0.9223682, 1 : 0.0752954, 2 : 0.002305, 3 : 0.0000314, 4 : 0.0000002 }
+
+    actual = { k:round(v,11) for k,v in probabilitycalc.prob_for_exactly_x(0.01, 7).iteritems() }
+    assert actual == { 0 : 0.93206534791, 1 : 0.06590361046, 2 : 0.0019970791, 3 : 0.00003362086, 4 : 0.0000003396, 5 : 0.00000000206 }
+
+
+def test_at_least_x():
+    prob_exactly_x = { 0 : 0.15, 1 : 0.45, 2 : 0.25, 3 : 0.10, 4 : 0.05 }
+    at_least_x = { k:round(v,5) for k,v in probabilitycalc.at_least_x(prob_exactly_x).iteritems() }
+    expected = { 0 : 1.0, 1 : 0.85, 2 : 0.4, 3 : 0.15, 4 : 0.05 }
+    
+    assert at_least_x == expected
+
+
+def test_aggregating_prob_exactly_x():
+    prob_dict = { 0.03 : { 0 : 0.55, 1 : 0.35, 2 : 0.10 }, 0.05 : { 0 : 0.95, 1 : 0.05 } }
+    agg_prob_exactly_x = { k:round(v,5) for k,v in probabilitycalc.aggregate_prob_exactly_x(prob_dict).iteritems() }
+    expected = { 0 : 0.5225, 1 : 0.36, 2 : 0.1125, 3 : 0.005 }
+
+    assert agg_prob_exactly_x == expected
+
+    prob_dict = { 1.0 : { 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 1 }, 0.05 : { 0 : 0.95, 1 : 0.05 } }
+    actual = { k:round(v,5) for k,v in probabilitycalc.aggregate_prob_exactly_x(prob_dict).iteritems() }
+
+    assert actual == { 0 : 0, 1 : 0, 2 : 0, 3 : 0, 4 : 0.95, 5 : 0.05 }
+    
---
+++ pstepup/test/test_stepupbanner.py
@@ -0,0 +1,65 @@
+
+import copy
+
+from pstepup import stepupbanner, appdata
+
+
+def test_setting_ex_rainbow_rates():
+    banner_info = { 'bannerRainbowCount' : 1, 'fiveStarBaseUnitCount' : 20, 'totalAvailableLaps' : 1, 'steps' : { '1' : { 'summons' : { 'guaranteedGold' : 1, 'regular' : 10 } } } }
+    pull_types_and_rates = stepupbanner.StepUpBanner(appdata.pull_type, banner_info)
+    assert pull_types_and_rates.base_rates['guaranteedEXRainbow']['bannerRainbowRate'] == 0.05
+
+
+def test_all_pulls_accounted_for():
+    banner_info = { 'bannerRainbowCount' : 1, 'totalAvailableLaps' : 1, 'fiveStarBaseUnitCount' : 40, 'steps' : { '1' : { 'summons' : { 'guaranteedGold' : 1, 'regular' : 10 } }, '2' : { 'summons' : { 'guaranteedGold' : 1, 'regular' : 10 } } } }
+    total_pulls, expected_pulls = 0, 0
+    errors = []
+
+    step_up = stepupbanner.StepUpBanner(copy.deepcopy(appdata.pull_type), banner_info)
+
+    for step_number, step_data in banner_info['steps'].iteritems():
+        for pull_type, pull_count in step_data['summons'].iteritems():
+            expected_pulls += pull_count
+
+    for name, rate_dict in step_up.rates_and_pulls_per_lap.iteritems():
+        for key, value in rate_dict.iteritems():
+            total_pulls += value
+        if total_pulls != expected_pulls:
+            errors.append(name + ': unexpected pull count (' + str(total_pulls) + ') vs expected count (' + str(expected_pulls) + ')' )
+        total_pulls = 0
+
+    assert not errors, 'errors occured:\n{}'.format('\n'.join(errors))
+
+
+def test_no_rates_greater_than_100():
+    banner_info = { 'bannerRainbowCount' : 1, 'totalAvailableLaps' : 1, 'fiveStarBaseUnitCount' : 100, 'steps' : { '1' : { 'summons' : { 'guaranteedGold' : 1, 'regular' : 10 } }, '2' : { 'bonusBannerFiveStarRate' : 5, 'summons' : { 'guaranteedGold' : 1, 'regular' : 9, 'guaranteedRainbowWithRateUp' : 1 } } } }
+
+    expected_general_rates_and_pulls = { 0.03 : 10, 0.05 : 1, 0.2 : 1, 0.07 : 9, 1.0 : 1 }
+    expected_banner_rates_and_pulls = { 0.01 : 10, 0.0375 : 1, 0.1875 : 1, 0.05 : 9, 0.25 : 1 }
+    expected_oneunit_rates_and_pulls = expected_banner_rates_and_pulls
+
+    step_up = stepupbanner.StepUpBanner(copy.deepcopy(appdata.pull_type), banner_info)
+
+    assert expected_general_rates_and_pulls == step_up.rates_and_pulls_per_lap['general']
+    assert expected_banner_rates_and_pulls == step_up.rates_and_pulls_per_lap['banner'] 
+    assert expected_oneunit_rates_and_pulls == step_up.rates_and_pulls_per_lap['oneunit']
+
+
+def test_sum_pulls_for_rates():
+    banner_info = { 'bannerRainbowCount' : 1, 'totalAvailableLaps' : 1, 'fiveStarBaseUnitCount' : 100, 'steps' : { '1' : { 'summons' : { 'guaranteedGold' : 1, 'regular' : 10 } }, '2' : { 'summons' : { 'guaranteedGold' : 1, 'regular' : 10 } } } }
+    rates_and_pulls = stepupbanner.sum_pulls_for_rates(copy.deepcopy(appdata.pull_type), banner_info)
+
+    assert rates_and_pulls['general'] == { 0.03 : 20, 0.05 : 2 }
+    assert rates_and_pulls['banner'] == { 0.01 : 20, 0.0375 : 2 }
+    assert rates_and_pulls['oneunit'] == { 0.01 : 20, 0.0375 : 2 }
+
+    two_laps = { type_rate: { rate:pull*2 for rate, pull in r_and_p.iteritems() } for type_rate, r_and_p in rates_and_pulls.iteritems() }
+
+    assert two_laps['general'] == { 0.03 : 40, 0.05 : 4 }
+    assert two_laps['banner'] == { 0.01 : 40, 0.0375 : 4 }
+    assert two_laps['oneunit'] == { 0.01 : 40, 0.0375 : 4 }
+
+    banner_info['bannerRainbowCount'] = 2
+    multi_banner = stepupbanner.sum_pulls_for_rates(copy.deepcopy(appdata.pull_type), banner_info)
+
+    assert multi_banner['oneunit'] == { 0.005 : 20, 0.01875 : 2 }
