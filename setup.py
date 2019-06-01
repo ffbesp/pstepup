2d3285b48fb10abc28756d22bd248a48cc0d8915
Stu Baker <stubakers@gmail.com>
2019-05-31 18:13:38 -0600

    adding requirements, setup and init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pstepup',
+      version='0.1',
+      description='Probability for laps on FFBE step-up summon banners',
+      url='https://github.com/ffbesp/pstepup',
+      author='Stu Baker (@stuaroo)',
+      author_email='stubakers@gmail.com',
+      license='MIT',
+      packages=['pstepup'],
+      zip_safe=False)
