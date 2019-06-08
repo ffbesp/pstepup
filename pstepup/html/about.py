6a0ff8d9b8b7b9581ef21ffe37de6bdf6e93eba7
Stu Baker <stubakers@gmail.com>
2019-06-07 18:59:09 -0600

    updating about, bannerlist for linting

--- pstepup/html/about.py
+++ pstepup/html/about.py
@@ -1,3 +1,5 @@
+"""Creates About HTML page on file system at ../../../ffbesp.github.io/about/index.html
+"""
 
 import os
 
@@ -7,18 +9,26 @@ import home
 
 
 def main():
-    template_vars = { 'title' : 'About - ' + sitesettings.SITE_NAME,
-                    'siteurl' : sitesettings.SITE_URL,
-                    'sitename' : sitesettings.SITE_NAME,
-                    'meta_desc' : 'Expected values and probability per lap of step-up banners in Final Fantasy Brave Exvius (FFBE)',
-                    'last_four_banners' : home.get_last_four_banners('all'), }
+    meta_desc = ('Expected values and probability per lap of step-up banners'
+        ' in Final Fantasy Brave Exvius (FFBE)')
+    template_vars = {
+        'title' : 'About - ' + sitesettings.SITE_NAME,
+        'siteurl' : sitesettings.SITE_URL,
+        'sitename' : sitesettings.SITE_NAME,
+        'meta_desc' : meta_desc,
+        'last_four_banners' : home.get_last_four_banners('all'),
+    }
 
-    if not os.path.exists(os.path.join(os.getcwd(), os.pardir, os.pardir, os.pardir, 'ffbesp.github.io', 'about')):
-        os.makedirs(os.path.join(os.getcwd(), os.pardir, os.pardir, os.pardir, 'ffbesp.github.io', 'about'))
+    about_path = os.path.join(
+        os.getcwd(), os.pardir, os.pardir, os.pardir, 'ffbesp.github.io', 'about')
+
+    if not os.path.exists(about_path):
+        os.makedirs(about_path)
 
     template_file = 'about.html'
-    html_file_loc = os.path.join(os.getcwd(), os.pardir, os.pardir, os.pardir, 'ffbesp.github.io', 'about', 'index.html')
-    generatehtml.generate_html(html_file_loc, template_file, template_vars, os.path.join(os.getcwd(), 'templates'))
+    html_file_loc = os.path.join(about_path, 'index.html')
+    generatehtml.generate_html(
+        html_file_loc, template_file, template_vars, os.path.join(os.getcwd(), 'templates'))
 
 
 
