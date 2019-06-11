26dd91c4c33519dd42019d34c2b0b21d17bc5ce4
Stu Baker <stubakers@gmail.com>
2019-06-11 03:18:10 -0600

    moving functions from home to generate for HTML
    also added default local location path to sitesettings file

--- pstepup/html/generate.py
+++ pstepup/html/generate.py
@@ -1,33 +1,78 @@
 """Creates HTML pages for project
 
-Default file location is adjacent to this project; from this
-location: ../../../ffbesp.github.io/
+Default file location is adjacent to this project
 """
 
 import os
+import json
 
 import appdata
-import home
 import banner
 import sitesettings
 import generatehtml
 
 
-def create_about():
-    """Creates About HTML page on file system at ../../../ffbesp.github.io/about/index.html
+def get_last_four_banners(banner_type):
+    """Find last 4 banners in reverse chronological order
+
+    Args:
+        banner_type (str): types of banner to return (single, multi, or all)
     """
-    meta_desc = ('Expected values and probability per lap of step-up banners'
+    banner_list = []
+    for banner_id, banner_details in appdata.banner_info.items():
+        combined_banner_info = banner_details
+        combined_banner_info['banner_id'] = banner_id
+        banner_list.append(combined_banner_info)
+
+    banner_sort = sorted(
+        banner_list, key=lambda k: k['duration']['start'].replace('-', ''), reverse=True)
+
+    if banner_type == 'single':
+        return [x for x in banner_sort if len(x['banner_icon']) == 1][:4]
+    elif banner_type == 'multi':
+        return [x for x in banner_sort if len(x['banner_icon']) > 1][:4]
+    else: #all
+        return banner_sort[:4]
+
+
+def get_all_banner_info():
+    """Find banner metadata for all known banners
+
+    Returns:
+        List of all banners in reverse chronological order
+    """
+    all_banner_info = []
+    for banner_id, banner_details in appdata.banner_info.items():
+        combined_banner_info = banner_details
+        combined_banner_info['banner_id'] = banner_id
+        #if file exists, grab non-step details
+        b_file = os.path.join(os.getcwd(), os.pardir, 'banner', banner_details['banner_json'])
+        if os.path.isfile(b_file):
+            with open(b_file, 'r') as banner_file:
+                banner_json = json.load(banner_file)
+            for key, value in banner_json.items():
+                if not key == 'steps':
+                    combined_banner_info[key] = value
+        all_banner_info.append(combined_banner_info)
+    return sorted(
+        all_banner_info, key=lambda k: k['duration']['start'].replace('-', ''), reverse=True)
+
+
+def create_about():
+    """Creates About HTML page
+    """
+    meta_desc = (
+        'Expected values and probability per lap of step-up banners'
         ' in Final Fantasy Brave Exvius (FFBE)')
     template_vars = {
         'title' : 'About - ' + sitesettings.SITE_NAME,
         'siteurl' : sitesettings.SITE_URL,
         'sitename' : sitesettings.SITE_NAME,
         'meta_desc' : meta_desc,
-        'last_four_banners' : home.get_last_four_banners('all'),
+        'last_four_banners' : get_last_four_banners('all'),
     }
 
-    about_path = os.path.join(
-        os.getcwd(), os.pardir, os.pardir, os.pardir, 'ffbesp.github.io', 'about')
+    about_path = os.path.join(sitesettings.LOCAL_FILE_PATH, 'about')
 
     if not os.path.exists(about_path):
         os.makedirs(about_path)
@@ -39,19 +84,18 @@ def create_about():
 
 
 def create_banner_list():
-    """Creates Banner List HTML page on file system at ../../../ffbesp.github.io/banner/index.html
+    """Creates Banner List HTML page
     """
     template_vars = {
         'title' : 'Banners - ' + sitesettings.SITE_NAME,
         'siteurl' : sitesettings.SITE_URL,
         'sitename' : sitesettings.SITE_NAME,
         'meta_desc' : 'List of step-up banners in Final Fantasy Brave Exvius (FFBE)',
-        'last_four_banners' : home.get_last_four_banners('all'),
-        'all_banner_info' : home.get_all_banner_info(), 
+        'last_four_banners' : get_last_four_banners('all'),
+        'all_banner_info' : get_all_banner_info(),
     }
 
-    bn_path = os.path.join(
-        os.getcwd(), os.pardir, os.pardir, os.pardir, 'ffbesp.github.io', 'banner')
+    bn_path = os.path.join(sitesettings.LOCAL_FILE_PATH, 'banner')
 
     if not os.path.exists(bn_path):
         os.makedirs(bn_path)
@@ -62,8 +106,38 @@ def create_banner_list():
         html_file_loc, template_file, template_vars, os.path.join(os.getcwd(), 'templates'))
 
 
+def create_home():
+    """Creates Home HTML page
+    """
+    meta_desc = (
+        'Expected values and probability per lap of step-up'
+        ' banners in Final Fantasy Brave Exvius (FFBE)')
+    template_vars = {
+        'title' : sitesettings.SITE_NAME,
+        'siteurl' : sitesettings.SITE_URL,
+        'sitename' : sitesettings.SITE_NAME,
+        'meta_desc' : meta_desc,
+        'last_four_banners' : get_last_four_banners('all'),
+        'last_four_single' : get_last_four_banners('single'),
+        'last_four_multi' : get_last_four_banners('multi'),
+        'all_banner_info' : get_all_banner_info(),
+    }
+
+    home_path = os.path.join(sitesettings.LOCAL_FILE_PATH)
+
+    if not os.path.exists(home_path):
+        os.makedirs(home_path)
+
+    template_file = 'home.html'
+    html_file_loc = os.path.join(home_path, 'index.html')
+    generatehtml.generate_html(
+        html_file_loc, template_file, template_vars, os.path.join(os.getcwd(), 'templates'))
+
+
 def main():
-    home.main()
+    """Generate basic website HTML pages, then pages for all available banners
+    """
+    create_home()
     create_about()
     create_banner_list()
 
--- pstepup/html/sitesettings.py
+++ pstepup/html/sitesettings.py
@@ -1,3 +1,9 @@
 
+import os
+
+
 SITE_URL = 'https://ffbesp.github.io'
 SITE_NAME = 'FFBE Step-up Probability'
+
+#Default location is adjacent to project (../../../ffbesp.github.io/)
+LOCAL_FILE_PATH = os.path.join(os.getcwd(), os.pardir, os.pardir, os.pardir, 'ffbesp.github.io')
