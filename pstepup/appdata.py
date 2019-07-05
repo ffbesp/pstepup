522a33689d3b0f394824d8ae21b55629803699a0
Stu Baker <stubakers@gmail.com>
2019-07-05 12:05:53 -0600

    adding SS Charlotte banner
    - updating Regina banner for wrong trust moogle on step 1

--- pstepup/appdata.py
+++ pstepup/appdata.py
@@ -28,6 +28,15 @@ pull_type = {
 
 #only step-up banners; types = limited_time_collab, limited_time_seasonal, new_permanent_units, existing_permanent_units
 banner_info = {
+    "sscharlotte" : {
+        "banner_name" : "SS Charlotte 24K",
+        "banner_type" : "new_permanent_units",
+        "duration" : { "start" : "2019-07-05" , "end" : "2019-07-19" },
+        "news_image_link" : "https://lapis-prod-staticnews-gumi-sg.akamaized.net/prod//en/img/CharlotteFeaturedSummon.jpg",
+        "banner_json" : "sscharlotte.json",
+        "banner_icon" : { "SS Charlotte" : "100021707" },
+        "banner_img" : "sscharlotte.jpg"
+    },
     "kayaka-daileen" : {
         "banner_name" : "K. Ayaka/D. Aileen 25K",
         "banner_type" : "new_permanent_units",
