4934f7e752cc39bca479f6ff97145e087018051a
Stu Baker <stubakers@gmail.com>
2019-07-29 18:56:05 -0600

    adding Rivera banner

--- pstepup/appdata.py
+++ pstepup/appdata.py
@@ -28,6 +28,15 @@ pull_type = {
 
 #only step-up banners; types = limited_time_collab, limited_time_seasonal, new_permanent_units, existing_permanent_units
 banner_info = {
+    "rivera" : {
+        "banner_name" : "Rivera 12K",
+        "banner_type" : "new_permanent_units",
+        "duration" : { "start" : "2019-07-26" , "end" : "2019-08-09" },
+        "news_image_link" : "https://lapis-prod-staticnews-gumi-sg.akamaized.net/prod//en/img/gacha_NEWS_RiveraFeatureSummon_Compilation.jpg",
+        "banner_json" : "rivera.json",
+        "banner_icon" : { "Rivera" : "401007707" },
+        "banner_img" : "rivera.jpg"
+    },
     "elena" : {
         "banner_name" : "Elena 20K",
         "banner_type" : "new_permanent_units",
