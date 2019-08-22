566790bfe6d223ffcc7c86c57f7bc0b419995263
Stu Baker <stubakers@gmail.com>
2019-08-22 17:22:26 -0600

    adding Kaito, Tsukiko banner

--- pstepup/appdata.py
+++ pstepup/appdata.py
@@ -28,6 +28,15 @@ pull_type = {
 
 #only step-up banners; types = limited_time_collab, limited_time_seasonal, new_permanent_units, existing_permanent_units
 banner_info = {
+    "kaito-tsukiko" : {
+        "banner_name" : "Kaito/Tsukiko 24K",
+        "banner_type" : "new_permanent_units",
+        "duration" : { "start" : "2019-08-16" , "end" : "2019-08-30" },
+        "news_image_link" : "https://lapis-prod-staticnews-gumi-sg.akamaized.net/prod//en/img/KaitoTsukikoGacha3.jpg",
+        "banner_json" : "kaito-tsukiko.json",
+        "banner_icon" : { "Kaito" : "401008507", "Tsukiko" : "401007107" },
+        "banner_img" : "kaito-tsukiko.jpg"
+    },
     "rlightning-nhope" : {
         "banner_name" : "R. Lightning/N. Hope 25K",
         "banner_type" : "new_permanent_units",
